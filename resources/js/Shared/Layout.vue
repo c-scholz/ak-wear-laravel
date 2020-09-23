4abf01a0e685c7e48898334b6c832ee7619378f6
c-scholz <scholzcedric@web.de>
2020-09-23 13:01:12 +0200

    implemented basic templating

---
+++ resources/js/Shared/Layout.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <main-menu />
+    <slot />
+  </div>
+</template>
+
+<script>
+import MainMenu from '../Shared/MainMenu'
+
+export default {
+  components: { 
+    MainMenu 
+  }
+}
+</script>
