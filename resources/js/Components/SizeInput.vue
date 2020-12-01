4ca788bfec404c7ce0a4c9c99c000f1de02b1b44
c-scholz <scholzcedric@web.de>
2020-12-01 21:48:00 +0100

    hiding arrows in size input, fixed bug w/ focusout

--- resources/js/Components/SizeInput.vue
+++ resources/js/Components/SizeInput.vue
@@ -2,7 +2,7 @@
   <div class="col-9 col-md-6">
     <div class="groessenAngabe">
       <span class="input-number-minus" @click="reduce()">–</span>
-      <input class="input-number" :name="name" type="number" :value="value" min="0">
+      <input class="input-number" :name="name" type="number" :value="value" min="0" @focusout="update($event)">
       <span class="input-number-plus" @click="add()">+</span>
     </div>
   </div>
@@ -26,7 +26,21 @@ export default {
       if(this.value > 0) {
         this.value -= 1;
       }
+    },
+    update(event) {
+      this.value = Number(event.target.value);
     }
   }
 }
 </script>
+
+<style scoped>
+input::-webkit-outer-spin-button,
+input::-webkit-inner-spin-button {
+   -webkit-appearance: none;
+   margin: 0;
+}
+input[type="number"] {
+   -moz-appearance: textfield;
+}
+</style>
